3d6a8b2ae5ee046d73166626e401b7621570fd99
e.omelchenko <omel4encko.evgenya@yandex.ru>
2023-02-27 13:01:36 +0300

    fix: исправлена работа с rate limits

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
 const pageNotFound = require('./middlewares/pageNotFound');
 const customErrors = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const index = require('./routes/index');
+const { rateLimits } = require('./utils/rateLimiterConfig');
 
-const { PORT = 3000, DB_SERVER_URL = 'mongodb://localhost:27017/anyfilmsdb' } = process.env;
+const { PORT = 3000, DB_SERVER_URL = 'mongodb://localhost:27017/anyfilmsdb' } =
+  process.env;
 
 const app = express();
+app.use(rateLimit(rateLimits));
 app.use(cookieParser());
 app.use(express.json());
 app.use(requestLogger);
--- middlewares/rateLimiter.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require('express-rate-limit');
-const constants = require('../utils/constants');
-
-exports.rateLimiter = rateLimit({
-  windowMs: constants.PERIOD_FOR_IP,
-  max:constants.MAX_TRY_COUNT,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
